eec1c1034d04f6a4f85c07f689cf6386a448f344
Brendan Kane <54537957+FlerBaDet@users.noreply.github.com>
2024-10-26 15:32:18 -0400

    FogStart and EnemySpawning

---
+++ SpookyShapes_2024/Assets/Scripts/Enemy/EnemyTrenchSpawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrenchSpawn : MonoBehaviour
+{
+    
+    [SerializeField] GameObject enemyFight;
+    [SerializeField] GameObject enemyFightPos;
+    bool willBeActive;
+    bool waiting;
+
+    private void Start()
+    {
+        willBeActive = Random.Range(0, 10) <= 2 ? true : false;
+        if (!willBeActive || GameManager.Instance == null) gameObject.SetActive(false);
+        enemyFight.transform.position = enemyFightPos.transform.position;
+    }
+}
